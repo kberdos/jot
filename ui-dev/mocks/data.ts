b72ce1b8251887ed591ff92d631b5febcaf2c91f
rdbrooks <rachel_brooks@brown.edu>
2026-04-22 14:07:32 -0400

    created frontend branch and structure

---
+++ ui-dev/mocks/data.ts
@@ -0,0 +1,12 @@
+import { Note } from "../../util/objects/note";
+
+export const mockNote: Note = {
+  id: "1",
+  x: 100,
+  y: 150,
+  width: 200,
+  height: 120,
+  color: "#FFEB3B",
+  board_id: "demo-board",
+  author_id: "user-1",
+};
