ca0204f1aaaa67d83ea1e775f3fa98aa1c89d881
rdbrooks <rachel_brooks@brown.edu>
2026-04-23 14:42:04 -0400

    starting to create global

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,35 @@
-"use client"
+"use client";
 
 import { login, logout, useAuthStore } from "@/util/auth/auth";
 import { useBoardStore } from "@/util/objects/board";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const { user } = useAuthStore()
-  const { createBoard } = useBoardStore()
-  const router = useRouter()
+  const { user } = useAuthStore();
+  const { createBoard } = useBoardStore();
+  const router = useRouter();
 
-  const viewBoards = () => router.push("/boards")
+  const viewBoards = () => router.push("/boards");
 
   const handleNewBoard = async () => {
-    const name = "New Board"
-    const board = await createBoard(name, user!)
+    const name = "New Board";
+    const board = await createBoard(name, user!);
     // user HAS to be specified at this point
-    console.log("board id: ", board.id)
+    console.log("board id: ", board.id);
     // await saveNotes(board.id, user!)
-    router.push(`/boards/${board.id}`)
-  }
+    router.push(`/boards/${board.id}`);
+  };
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-white">
-
       {user ? (
         <div className="text-center space-y-6">
           <p className="text-gray-600">Welcome, {user.email}</p>
 
           <div className="flex flex-col gap-4 items-center">
-            <button
-              onClick={viewBoards}
-            >
-              Your Boards
-            </button>
+            <button onClick={viewBoards}>Your Boards</button>
 
-            <button
-              onClick={handleNewBoard}
-            >
-              New Board
-            </button>
+            <button onClick={handleNewBoard}>New Board</button>
 
             <button
               onClick={logout}
@@ -50,7 +41,6 @@ export default function Home() {
         </div>
       ) : (
         <div className="text-center space-y-8">
-
           {/* Title */}
           <h1
             className="text-7xl italic"
@@ -68,16 +58,11 @@ export default function Home() {
           </p>
 
           {/* Button */}
-          <button
-            onClick={login}
-            className="mt-6 px-8 py-3 rounded-xl text-black font-medium
-                       bg-[#8FD2FF] hover:bg-[#4BA7EF] transition shadow-sm"
-          >
+          <button onClick={login} className="blue-button">
             Sign in with Google
           </button>
         </div>
       )}
-
     </div>
-  )
+  );
 }
