9825947c35c08d1ef4d4f8f3853fbb6950d090f8
Kazuya Erdos <kazuya_erdos@brown.edu>
2026-04-22 23:39:01 -0400

    joke

--- app/api/gemini/route.ts
+++ app/api/gemini/route.ts
@@ -5,7 +5,7 @@ const ai = new GoogleGenAI({});
 export async function GET() {
 	const response = await ai.models.generateContent({
 		model: "gemini-3-flash-preview",
-		contents: "Explain how AI works in a few words",
+		contents: "tell me a joke",
 	});
 	return Response.json({ text: response.text });
 }
