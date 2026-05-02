28df3fc33f3e070f49d5da6c3ac4f41e4a3c19b1
rdbrooks <rachel_brooks@brown.edu>
2026-05-02 04:40:24 -0400

    edited styling of collab cursor

--- components/Collab.tsx
+++ components/Collab.tsx
@@ -33,16 +33,36 @@ export default function CollabLayer() {
 						position: "absolute",
 						left: cursor.x,
 						top: cursor.y,
+						zIndex: 800,
 					}}
 						key={cursor.user_id}
 					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="19"
+							height="20"
+							viewBox="0 0 19 20"
+							fill="none"
+							style={{
+							position: "absolute",
+							left: 0,
+							top: 0,
+							}}
+						>
+							<path
+							d="M2.20294 0.178274C1.09955 -0.407015 -0.192737 0.533679 0.0239761 1.76272L2.97756 18.5133C3.22449 19.9137 5.10057 20.2165 5.77538 18.9643L8.98139 13.0173C9.09972 12.7981 9.26615 12.6085 9.4682 12.4628C9.67024 12.3171 9.90266 12.219 10.148 12.1759L16.8959 10.9861C18.3022 10.7381 18.5998 8.84972 17.3369 8.18291L2.20294 0.178274Z"
+							fill="black"
+							/>
+						</svg>
 						<div style={{
+							position: "absolute",
 							backgroundColor: color,
-							width: `20px`,
-							height: `20px`,
+							left: 12,
+      						top: 16,
+							display: "inline-block",
 						}}
 						>
-							{cursor.user_name}
+							{cursor.user_email}
 						</div>
 					</div>
 				)
